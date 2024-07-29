3db2db5d350744b794ef18978adecf91008b3dd2
Krishankant Saraswat <122577473+KishankantSaraswat@users.noreply.github.com>
2024-07-29 16:01:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 # This block is only used for local development
 if __name__ == '__main__':
